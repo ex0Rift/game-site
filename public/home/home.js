b72e03a7513f45e402941de10d0800f8d9c738b6
Nate <daviesnathanael45@gmail.com>
2026-02-16 12:58:19 +0000

    filters now work and the renderGamelist is a function so the gameList can be reloaded without need for page refresh

--- public/home/home.js
+++ public/home/home.js
@@ -1,39 +1,78 @@
 const grid = document.getElementById('grid');
 
-//make the game list from JSON file
-fetch('../data/gameList.json')
-    .then (res => res.json())
-    .then (data => {
-        //this code runs per item in list of games
-        data.forEach(item => {
-            //makes the DIVS
-            const div = document.createElement('div');
-            //gives it the classes
-            div.className = 'item basic-style';
-            //adds the HTML
-            div.innerHTML = `
-                <img src="${item.thumbnail}">
-                <div class="item-text-section">
-                    <h2>${item.title}</h2>
-                    <p>${item.description}</p>
-                </div>
-                <div class="item-attribute-row">
-                    <h4>${item.type}</h4>
-                </div>
-            `;
-            //change background colour of item.type depeding on paltform
-            if (item.type === "web"){
-                div.querySelector("h4").style.backgroundColor = "#0b72e9ff";
-            } else {
-                div.querySelector("h4").style.backgroundColor = "#e91a0b";
-            }
+let filters = [];
 
-            //add event listener to redirect when pressed
-            div.addEventListener("click", () =>{
-                window.location.href = item.location;
+function renderGameList(){
+    //make the game list from JSON file
+    fetch('../data/gameList.json')
+        .then (res => res.json())
+        .then (data => {
+            //this code runs per item in list of games
+            data.forEach(item => {
+
+                //check if this item is in filters
+                let show = false;
+                filters.forEach(filter =>{
+                    if (filter === item.type){
+                        show = true;
+                    }
+                });
+
+                //if filters is empyty shows all
+                if (filters.length === 0) show = true;
+                //skips this div if filters dont pass with any true
+                if (!show) return;
+
+                //makes the DIVS
+                const div = document.createElement('div');
+                //gives it the classes
+                div.className = 'item basic-style';
+                //adds the HTML
+                div.innerHTML = `
+                    <img src="${item.thumbnail}">
+                    <div class="item-text-section">
+                        <h2>${item.title}</h2>
+                        <p>${item.description}</p>
+                    </div>
+                    <div class="item-attribute-row">
+                        <h4>${item.type}</h4>
+                    </div>
+                `;
+                //change background colour of item.type depeding on paltform
+                if (item.type === "web"){
+                    div.querySelector("h4").style.backgroundColor = "#0b72e9ff";
+                } else {
+                    div.querySelector("h4").style.backgroundColor = "#e91a0b";
+                }
+
+                //add event listener to redirect when pressed
+                div.addEventListener("click", () =>{
+                    window.location.href = item.location;
+                });
+
+                //Puts it inside the grid element
+                grid.appendChild(div);
             });
-
-            //Puts it inside the grid element
-            grid.appendChild(div);
         });
-    });
+}
+
+function addFilter(button, type){
+    //change teh button to look "in use"
+    button.style.backgroundColor = "#8f8f8f";
+
+    //check if item is already in filters
+    if (!filters.includes(type)){
+        //if not add to filters
+        filters.push(type);
+    } else{
+        //if is then remove it from filiters and put button back to normal
+        filters = filters.filter(item => item !== type);
+        button.style.backgroundColor = "#303030";
+    }
+    //reset the gameList
+    grid.innerHTML = "";
+    renderGameList();
+}
+
+
+renderGameList();
